4e9b3c1332d6653c3a32e941e19f157d746fa0fd
thaMilo <milougo.galli@gmail.com>
2024-05-06 16:43:56 +0200

    almost done with the second part of second assignment

--- Assignment_2/PART_2_DYNAMIC_DISASM/dynamic_disasm.c
+++ Assignment_2/PART_2_DYNAMIC_DISASM/dynamic_disasm.c
@@ -1,17 +1,56 @@
-#include <errno.h> // For error handling
+#include <capstone/capstone.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
-#include <sys/user.h> // For accessing the instruction pointer
+#include <sys/user.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+void get_instruction_pointer(pid_t pid, unsigned long *rip) {
+  struct user_regs_struct regs;
+  if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) == 0) {
+    *rip = regs.rip;
+  } else {
+    perror("ptrace(PTRACE_GETREGS)");
+  }
+}
+
+void get_assembly_mnemonics(pid_t pid, unsigned long rip) {
+  csh handle;
+  cs_insn *insn;
+  size_t count;
+  long data = ptrace(PTRACE_PEEKDATA, pid, rip, NULL);
+  unsigned char code[8];
+  memcpy(code, &data, sizeof(data));
+  char *assembly_mnemonics = NULL;
+
+  if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
+    fprintf(stderr, "Failed to initialize Capstone\n");
+  }
+
+  count = cs_disasm(handle, code, sizeof(code), rip, 0, &insn);
+
+  if (count > 0) {
+    for (size_t i = 0; i < count; i++) {
+      printf("0x%lx: %s %s\n", insn[i].address, insn[i].mnemonic,
+             insn[i].op_str);
+    }
+    cs_free(insn, count);
+  } else {
+    fprintf(stderr, "Failed to disassemble instruction at 0x%lx\n", rip);
+  }
+  cs_close(&handle);
+}
 
 int trace_execution(char *program_to_execute,
                     char *program_to_execute_arguments[]) {
   pid_t pid;
   int status;
   long machine_code_instruction_num = 0;
+  unsigned long rip;
 
   pid = fork();
   if (pid == -1) {
@@ -32,6 +71,8 @@ int trace_execution(char *program_to_execute,
 
     while (WIFSTOPPED(status)) {
       machine_code_instruction_num++;
+      get_instruction_pointer(pid, &rip);
+      get_assembly_mnemonics(pid, rip);
       ptrace(PTRACE_SINGLESTEP, pid, NULL, NULL);
       waitpid(pid, &status, 0);
     }
