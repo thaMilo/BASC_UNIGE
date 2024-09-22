7705f882bfabf76e4ff7f62dee8dd2bdfab1a6c3
thaMilo <milougo.galli@gmail.com>
2024-09-22 16:04:55 +0200

    added infinite lives hack to toppler32

--- TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/patcher.py
+++ TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/patcher.py
@@ -2,8 +2,9 @@ from pwn import *
 
 if __name__ == "__main__":
     elf = ELF('./toppler32')
-    # target_address = elf.symbols['vulnerable_function']
-    # new_bytes = asm('ret')  # This assembles a 'ret' instruction
-    # elf.write(target_address, new_bytes)
-    # elf.save('./patched_binary')
-    print("Binary patched successfully!")
+    
+    # infinite lives patch
+    lifes_left_function = elf.symbols["_Z13pts_lifesleftv"]
+    patch_code = asm('mov al, 1; ret')
+    elf.write(lifes_left_function, patch_code)
+    elf.save('./toppler_patched')
