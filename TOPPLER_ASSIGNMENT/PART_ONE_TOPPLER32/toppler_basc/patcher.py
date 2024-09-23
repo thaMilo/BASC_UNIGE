b8d209638c26309ec7e43b1e5d79ec25ca35aba9
thaMilo <milougo.galli@gmail.com>
2024-09-23 15:16:15 +0200

    zero collisions hack

--- TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/patcher.py
+++ TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/patcher.py
@@ -3,14 +3,19 @@ from pwn import *
 if __name__ == "__main__":
     elf = ELF('./toppler32')
     
-    # infinite lives patch
+    # infinite lives
     lifes_left_function = elf.symbols["_Z13pts_lifesleftv"]
     patch_code = asm('mov al, 1; ret')
     elf.write(lifes_left_function, patch_code)
 
-    # time doesn't pass
+    # infinite time
     time_update_address = 0x0804c4c9
     new_instruction = asm('ret')
     elf.write(time_update_address, new_instruction)
-    
+
+    # no collisions
+    collisions_address = 0x0805cb01
+    new_instruction = asm('ret')
+    elf.write(collisions_address, new_instruction)
+        
     elf.save('./toppler_patched')
