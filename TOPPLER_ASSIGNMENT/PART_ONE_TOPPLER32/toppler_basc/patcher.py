af975513c6cb13b08a1acb65808f89c0ec8c8bc5
thaMilo <milougo.galli@gmail.com>
2024-09-23 14:56:32 +0200

    added infinite time glitch

--- TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/patcher.py
+++ TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/patcher.py
@@ -7,4 +7,10 @@ if __name__ == "__main__":
     lifes_left_function = elf.symbols["_Z13pts_lifesleftv"]
     patch_code = asm('mov al, 1; ret')
     elf.write(lifes_left_function, patch_code)
+
+    # time doesn't pass
+    time_update_address = 0x0804c4c9
+    new_instruction = asm('ret')
+    elf.write(time_update_address, new_instruction)
+    
     elf.save('./toppler_patched')
---
+++ TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/sas.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm toppler_patched
+python3 patcher.py
+chmod +x toppler_patched
