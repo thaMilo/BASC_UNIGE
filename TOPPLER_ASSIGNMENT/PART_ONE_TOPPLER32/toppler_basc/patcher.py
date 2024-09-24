bc83b94229bd7d81c6958496acf7145522f35472
thaMilo <milougo.galli@gmail.com>
2024-09-24 02:23:37 +0200

    fixed imports

--- TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/patcher.py
+++ TOPPLER_ASSIGNMENT/PART_ONE_TOPPLER32/toppler_basc/patcher.py
@@ -1,27 +1,27 @@
-from pwn import *
+from pwn import asm, ELF
 
 if __name__ == "__main__":
-    elf = ELF('./toppler32')
-    
+    elf = ELF("./toppler32")
+
     # infinite lives
     lifes_left_function = elf.symbols["_Z13pts_lifesleftv"]
-    patch_code = asm('mov al, 1; ret')
+    patch_code = asm("mov al, 1; ret")
     elf.write(lifes_left_function, patch_code)
 
     # infinite time
-    time_update_address = 0x0804c4c9
-    new_instruction = asm('ret')
+
+    time_update_address = 0x0804C4C9
+    new_instruction = asm("ret")
     elf.write(time_update_address, new_instruction)
 
     # no collisions
-    collisions_address = 0x0805cb01
-    new_instruction = asm('ret')
+    collisions_address = 0x0805CB01
+    new_instruction = asm("ret")
     elf.write(collisions_address, new_instruction)
 
     # no dropping of 1 layer
-    drown_address = 0x0805c056
-    address_to = 0x0805c014
-    new_instruction = asm(f'jmp {hex(address_to)}')
+    drown_address = 0x0805C00C
+    new_instruction = asm("add esi, 0x1")
     elf.write(drown_address, new_instruction)
-  
-    elf.save('./toppler_patched')
+
+    elf.save("./toppler_patched")
