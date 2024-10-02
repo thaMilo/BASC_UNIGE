049514bec675b6d3013cc0a233af97c9544d205d
thaMilo <milougo.galli@gmail.com>
2024-10-02 16:47:51 +0200

    last tweak todo

--- TOPPLER_ASSIGNMENT/Code/toppler64/patcher64.py
+++ TOPPLER_ASSIGNMENT/Code/toppler64/patcher64.py
@@ -1,26 +1,32 @@
-from pwn import asm, ELF
+from pwn import *
 
 if __name__ == "__main__":
     elf = ELF("./toppler64")
-
-    # # infinite lives
-    # lifes_left_function = elf.symbols["_Z13pts_lifesleftv"]
-    # patch_code = asm("mov al, 1; ret")
-    # elf.write(lifes_left_function, patch_code)
-    
-    # infinite lives
-    time_update_address = 0x00414008
+    context.update(arch='amd64')
+  
+    # lives do not decrease
+    time_update_address = 0x414008
     patch_code = asm("nop")
     elf.write(time_update_address, patch_code)
     
     # infinite time
-    time_update_address = 0x004063b2
+    time_update_address = 0x4063b2
     patch_code = asm("ret")
     elf.write(time_update_address, patch_code)
 
     # no enemies
     collisions_address = 0x414d9e
     patch_code = asm("ret")
+    elf.write(collisions_address, patch_code)
+
+
+    # starting with more lives than 3
+    collisions_address = 0x403e8f
+    patch_code = asm("mov dword ptr[rax+0x40], 0x9")
+    elf.write(collisions_address, patch_code)
+         
+    collisions_address = 0x404291
+    patch_code = asm("nop")
     elf.write(collisions_address, patch_code)
    
     elf.save("./toppler_patched")
