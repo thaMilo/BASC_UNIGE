00c6eabe378e8092d1208ffd5b64e675594ce642
thaMilo <milougo.galli@gmail.com>
2024-12-17 13:40:16 +0100

    working on it

--- Assignments/THE_LOCKS_ASSIGNMENT/exercises/lock2.py
+++ Assignments/THE_LOCKS_ASSIGNMENT/exercises/lock2.py
@@ -7,26 +7,20 @@ CODE_SIZE = 0x30000000
 STACK = 0x60000000
 STACK_SIZE = 0x10000000
 
-skip_list = []
-
 
 def hook_code(mu, address, size, user_data):
     print(f"[ INS - 0x{address:x} ] [ SIZE - {size}B ]")
-
-    # rdi = mu.reg_read(UC_X86_REG_RDI)
-    # print(hex(rdi))
 
     if address == 0x100010CF:
         mu.reg_write(UC_X86_REG_RIP, CODE + 0x1620)
 
     if address == 0x10001632:
-        mu.reg_write(UC_X86_REG_RIP, 0x10001733)
+        mu.reg_write(UC_X86_REG_RIP, CODE + 0x1733)
 
 
 def init_mu():
     mu = Uc(UC_ARCH_X86, UC_MODE_64)
     mu.mem_map(STACK, STACK_SIZE)
-
     mu.reg_write(UC_X86_REG_RSP, STACK + (STACK_SIZE // 2))
     return mu
 
@@ -40,10 +34,9 @@ if __name__ == "__main__":
     # setting up the code
     mu.mem_map(CODE, CODE_SIZE)
     mu.mem_write(CODE, code)
-
     # adding hook to trace instructions
     mu.hook_add(UC_HOOK_CODE, hook_code)
-
-    print(mu.mem_read(CODE + 0x17510, 25))
-    mu.emu_start(CODE + 0x10B0, CODE + 0x1751)
-    print(mu.mem_read(CODE + 0x17510, 25))
+    # starting the emulation
+    print(mu.mem_read(CODE + 0x13478, 10))
+    mu.emu_start(CODE + 0x10B0, CODE + 0x1651)
+    print(mu.mem_read(CODE + 0x17510, 25).decode("latin1"))
